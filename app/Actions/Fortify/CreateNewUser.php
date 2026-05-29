4a5faffc7d0ec555b76e774f10ef8c64a7a5c5bc
taka.h <hitoshi.t.biz@gmail.com>
2026-05-29 20:33:51 +0900

    feat: 認証機能の実装（Fortify） #3

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -6,7 +6,6 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
-use Illuminate\Validation\ValidationException;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 
 class CreateNewUser implements CreatesNewUsers
@@ -17,8 +16,6 @@ class CreateNewUser implements CreatesNewUsers
      * Validate and create a newly registered user.
      *
      * @param  array<string, string>  $input
-     *
-     * @throws ValidationException
      */
     public function create(array $input): User
     {
